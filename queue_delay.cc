228fa0d827e678d0351e9efe869a34b3af73af95
Ranysha Ware <rwjanee@gmail.com>
2018-01-17 13:58:54 -0500

    May or may not compile

--- queue_delay.cc
+++ queue_delay.cc
@@ -28,82 +28,247 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef BESS_MODULES_QUEUE_H_
-#define BESS_MODULES_QUEUE_H_
+#include "queue_delay.h"
 
-#include "../kmod/llring.h"
-#include "../module.h"
-#include "../pb/module_msg.pb.h"
+#include "../mem_alloc.h"
+#include "../utils/format.h"
 
-class Queue : public Module {
- public:
-  static const Commands cmds;
+#define DEFAULT_QUEUE_SIZE 1024
 
-  Queue()
-      : Module(),
-        queue_(),
-        prefetch_(),
-        backpressure_(),
-        burst_(),
-        size_(),
-        high_water_(),
-        low_water_(),
-        stats_() {
-    is_task_ = true;
-    propagate_workers_ = false;
-    max_allowed_workers_ = Worker::kMaxWorkers;
+const Commands QueueDelay::cmds = {
+    {"set_burst", "QueueCommandSetBurstArg",
+     MODULE_CMD_FUNC(&QueueDelay::CommandSetBurst), Command::THREAD_SAFE},
+    {"set_size", "QueueCommandSetSizeArg",
+     MODULE_CMD_FUNC(&QueueDelay::CommandSetSize), Command::THREAD_UNSAFE},
+    {"get_status", "QueueCommandGetStatusArg",
+     MODULE_CMD_FUNC(&QueueDelay::CommandGetStatus), Command::THREAD_SAFE},
+    {"set_delay", "QueueDelayCommandSetDelayArg",
+     MODULE_CMD_FUNC(&QueueDelay::CommandGetStatus), Command::THREAD_SAFE}};
+
+int QueueDelay::Resize(int slots) {
+  struct llring *old_queue = queue_;
+  struct llring *new_queue;
+
+  int bytes = llring_bytes_with_slots(slots);
+
+  int ret;
+
+  new_queue = static_cast<llring *>(mem_alloc_ex(bytes, alignof(llring), 0));
+  if (!new_queue) {
+    return -ENOMEM;
   }
 
-  CommandResponse Init(const bess::pb::QueueArg &arg);
+  ret = llring_init(new_queue, slots, 0, 1);
+  if (ret) {
+    mem_free(new_queue);
+    return -EINVAL;
+  }
 
-  void DeInit() override;
+  /* migrate packets from the old queue */
+  if (old_queue) {
+    bess::Packet *pkt;
 
-  struct task_result RunTask(void *arg) override;
-  void ProcessBatch(bess::PacketBatch *batch) override;
+    while (llring_sc_dequeue(old_queue, (void **)&pkt) == 0) {
+      ret = llring_sp_enqueue(new_queue, pkt);
+      if (ret == -LLRING_ERR_NOBUF) {
+        bess::Packet::Free(pkt);
+      }
+    }
 
-  std::string GetDesc() const override;
+    mem_free(old_queue);
+  }
 
-  CommandResponse CommandSetBurst(const bess::pb::QueueCommandSetBurstArg &arg);
-  CommandResponse CommandSetSize(const bess::pb::QueueCommandSetSizeArg &arg);
-  CommandResponse CommandGetStatus(
-      const bess::pb::QueueCommandGetStatusArg &arg);
+  queue_ = new_queue;
 
-  CheckConstraintResult CheckModuleConstraints() const override;
+  if (backpressure_) {
+    AdjustWaterLevels();
+  }
 
- private:
-  const double kHighWaterRatio = 0.90;
-  const double kLowWaterRatio = 0.15;
+  return 0;
+}
 
-  int Resize(int slots);
+CommandResponse QueueDelay::Init(const bess::pb::QueueDelayArg &arg) {
+  task_id_t tid;
+  CommandResponse err;
 
-  // Readjusts the water level according to `size_`.
-  void AdjustWaterLevels();
+  tid = RegisterTask(nullptr);
+  if (tid == INVALID_TASK_ID) {
+    return CommandFailure(ENOMEM, "Task creation failed");
+  }
 
-  CommandResponse SetSize(uint64_t size);
+  burst_ = bess::PacketBatch::kMaxBurst;
 
-  struct llring *queue_;
-  bool prefetch_;
+  if (arg.backpressure()) {
+    VLOG(1) << "Backpressure enabled for " << name() << "::QueueDelay";
+    backpressure_ = true;
+  }
 
-  // Whether backpressure should be applied or not
-  bool backpressure_;
+  if (arg.size() != 0) {
+    err = SetSize(arg.size());
+    if (err.error().code() != 0) {
+      return err;
+    }
+  } else {
+    size_ = DEFAULT_QUEUE_SIZE;
+    int ret = Resize(DEFAULT_QUEUE_SIZE);
+    if (ret) {
+      return CommandFailure(-ret);
+    }
+  }
 
-  int burst_;
+  if (arg.prefetch()) {
+    prefetch_ = true;
+  }
 
-  // Queue capacity
-  uint64_t size_;
+  return CommandSuccess();
+}
 
-  // High water occupancy
-  uint64_t high_water_;
+void QueueDelay::DeInit() {
+  bess::Packet *pkt;
 
-  // Low water occupancy
-  uint64_t low_water_;
+  if (queue_) {
+    while (llring_sc_dequeue(queue_, (void **)&pkt) == 0) {
+      bess::Packet::Free(pkt);
+    }
+    mem_free(queue_);
+  }
+}
 
-  // Accumulated statistics counters
-  struct {
-    uint64_t enqueued;
-    uint64_t dequeued;
-    uint64_t dropped;
-  } stats_;
-};
+std::string QueueDelay::GetDesc() const {
+  const struct llring *ring = queue_;
 
-#endif  // BESS_MODULES_QUEUE_H_
+  return bess::utils::Format("%u/%u", llring_count(ring), ring->common.slots);
+}
+
+/* from upstream */
+void QueueDelay::ProcessBatch(bess::PacketBatch *batch) {
+  int queued =
+      llring_mp_enqueue_burst(queue_, (void **)batch->pkts(), batch->cnt());
+  if (backpressure_ && llring_count(queue_) > high_water_) {
+    SignalOverload();
+  }
+
+  stats_.enqueued += queued;
+
+  if (queued < batch->cnt()) {
+    int to_drop = batch->cnt() - queued;
+    stats_.dropped += to_drop;
+    bess::Packet::Free(batch->pkts() + queued, to_drop);
+  }
+}
+
+/* to downstream */
+struct task_result QueueDelay::RunTask(void *) {
+  if (children_overload_ > 0) {
+    return {
+        .block = true, .packets = 0, .bits = 0,
+    };
+  }
+
+  bess::PacketBatch batch;
+
+  const int burst = ACCESS_ONCE(burst_);
+  const int pkt_overhead = 24;
+
+  uint64_t total_bytes = 0;
+
+  uint32_t cnt = llring_sc_dequeue_burst(queue_, (void **)batch.pkts(), burst);
+
+  if (cnt == 0) {
+    return {.block = true, .packets = 0, .bits = 0};
+  }
+
+  stats_.dequeued += cnt;
+  batch.set_cnt(cnt);
+
+  if (prefetch_) {
+    for (uint32_t i = 0; i < cnt; i++) {
+      total_bytes += batch.pkts()[i]->total_len();
+      rte_prefetch0(batch.pkts()[i]->head_data());
+    }
+  } else {
+    for (uint32_t i = 0; i < cnt; i++) {
+      total_bytes += batch.pkts()[i]->total_len();
+    }
+  }
+
+  RunNextModule(&batch);
+
+  if (backpressure_ && llring_count(queue_) < low_water_) {
+    SignalUnderload();
+  }
+
+  return {.block = false,
+          .packets = cnt,
+          .bits = (total_bytes + cnt * pkt_overhead) * 8};
+}
+
+CommandResponse QueueDelay::CommandSetBurst(
+    const bess::pb::QueueCommandSetBurstArg &arg) {
+  uint64_t burst = arg.burst();
+
+  if (burst > bess::PacketBatch::kMaxBurst) {
+    return CommandFailure(EINVAL, "burst size must be [0,%zu]",
+                          bess::PacketBatch::kMaxBurst);
+  }
+
+  burst_ = burst;
+  return CommandSuccess();
+}
+
+CommandResponse QueueDelay::SetSize(uint64_t size) {
+  if (size < 4 || size > 16384) {
+    return CommandFailure(EINVAL, "must be in [4, 16384]");
+  }
+
+  if (size & (size - 1)) {
+    return CommandFailure(EINVAL, "must be a power of 2");
+  }
+
+  int ret = Resize(size);
+  if (ret) {
+    return CommandFailure(-ret);
+  }
+  size_ = size;
+
+  return CommandSuccess();
+}
+
+CommandResponse QueueDelay::CommandSetSize(
+    const bess::pb::QueueCommandSetSizeArg &arg) {
+  return SetSize(arg.size());
+}
+
+CommandResponse QueueDelay::CommandGetStatus(
+    const bess::pb::QueueCommandGetStatusArg &) {
+  bess::pb::QueueCommandGetStatusResponse resp;
+  resp.set_count(llring_count(queue_));
+  resp.set_size(size_);
+  resp.set_enqueued(stats_.enqueued);
+  resp.set_dequeued(stats_.dequeued);
+  resp.set_dropped(stats_.dropped);
+  return CommandSuccess(resp);
+}
+
+void QueueDelay::AdjustWaterLevels() {
+  high_water_ = static_cast<uint64_t>(size_ * kHighWaterRatio);
+  low_water_ = static_cast<uint64_t>(size_ * kLowWaterRatio);
+}
+
+CheckConstraintResult QueueDelay::CheckModuleConstraints() const {
+  CheckConstraintResult status = CHECK_OK;
+  if (num_active_tasks() - tasks().size() < 1) {  // Assume multi-producer.
+    LOG(ERROR) << "QueueDelay has no producers";
+    status = CHECK_NONFATAL_ERROR;
+  }
+
+  if (tasks().size() > 1) {  // Assume single consumer.
+    LOG(ERROR) << "More than one consumer for the queue" << name();
+    return CHECK_FATAL_ERROR;
+  }
+
+  return status;
+}
+
+ADD_MODULE(QueueDelay, "queue",
+           "terminates current task and enqueue packets for new task")
