3b4bd14f67e84d3843d4b69aa10fed3fdf0e7ec1
rware <rware@andrew.cmu.edu>
2018-02-28 17:41:18 -0700

    Fix bug in kernel setup

--- setup-kernel.sh
+++ setup-kernel.sh
@@ -1,9 +1,10 @@
 # install latest version of iperf3
 wget http://downloads.es.net/pub/iperf/iperf-3-current.tar.gz
+tar -xzf iperf-3-current.tar.gz
 cd iperf-3.*
 sudo apt-get install lib32z1 # need this for some reason for iperf3
 ./configure && make && make install
-sudo rm -r iperf-3.4 iperf-3-current.tar.gz
+sudo rm -r iperf-3.* iperf-3-current.tar.gz
 
 # install new version of the kernel 4.13
 sudo wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.13/linux-headers-4.13.0-041300_4.13.0-041300.201709031731_all.deb
