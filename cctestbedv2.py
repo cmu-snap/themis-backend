c975fb65aa1b5838ff247aabca10219c5fe3ceae
Ranysha Ware <rware@andrew.cmu.edu>
2018-05-11 19:34:22 -0400

    Working iperf flows running after refactoring

--- cctestbedv2.py
+++ cctestbedv2.py
@@ -1,11 +1,14 @@
 from collections import namedtuple
 from datetime import datetime
-from contextlib import contextmanager
+from contextlib import contextmanager, ExitStack
 from logging.config import fileConfig
 from json import JSONEncoder
 import argparse
 import json
 import os
+import pwd
+import subprocess
+import shlex
 import time
 import logging
 import yaml
@@ -17,6 +20,45 @@ Host = namedtuple('Host', ['ifname', 'ip_wan', 'ip_lan', 'pci'])
 Flow = namedtuple('Flow', ['ccalg', 'start_time', 'end_time', 'rtt',
                            'server_port', 'client_port', 'client_log', 'server_log'])    
 
+# TODO: implement connect_dpdk function -- make sure driver is correct
+# TODO: implement functions for starting bess, queue collection
+# TODO: implement functions for starting tcpdump (issues with sudo?)
+
+# TODO: always pring 'Running cmd:{}'; wrapper around subprocess cmd
+# TODO: to set affinity, get number of processors on remote machines using 'nproc --all
+# TODO: check every seconds if processes still running instead of using time.sleep
+# TODO: allow starting and stopping flows at any time
+
+class BackgroundCommand:
+    def __init__(self, cmd, sudo=False,
+                 stdout='/dev/null', stdin='/dev/null', stderr='/dev/null',
+                 logs=[], username='ranysha'):
+        self.cmd = cmd
+        self.stdout = stdout
+        self.stdin = stdin
+        self.stderr = stderr
+        self.logs = logs
+        self.proc = None
+        self.username = username
+        self.sudo = sudo
+
+    @contextmanager
+    def __call__(self):
+        """
+            Reference: piping shell commands - https://docs.python.org/2/library/subprocess.html#replacing-shell-pipeline
+        """
+        try:
+            os.system('{} > {} 2> {} < {} &'.format(self.cmd,
+                                                    self.stdout,
+                                                    self.stderr,
+                                                    self.stdin))
+            proc = subprocess.run('pgrep {}'.format(cmd, stdout = subprocess.PIPE))
+            pid = proc.stdout
+        finally:
+            # kill pid
+            pass
+
+                                  
 class RemoteCommand:
     """Command to run on a remote machine in the background"""
     def __init__(self, cmd, ip_addr,
@@ -54,10 +96,11 @@ class RemoteCommand:
                                                                          self.stderr,
                                                                          self.stdin))
             ssh_client = self._get_ssh_client()
+            logging.info('Running cmd ({}): {}'.format(self.ip_addr, self.cmd))
             _, stdout, _ = ssh_client.exec_command('pgrep -f "{}"'.format(self.cmd.strip()))
             pid = stdout.readline().strip()
             assert(pid)
-            logging.info('Running cmd ({}): {}. \nPID={}'.format(self.ip_addr, self.cmd, pid))
+            logging.info('PID={}'.format(pid))
             # check if immediately get nonzero exit status
             if self._ssh_channel.exit_status_ready():
                 if self._ssh_channel.recv_exit_status() != 0:
@@ -104,6 +147,8 @@ class Experiment:
         self.queue_size = queue_size
         self.server = server
         self.client = client
+        # store what version of this code we are running -- could be useful later
+        self.git_commit = run_local_command('git rev-parse HEAD')
         self.logs = {
             'server_tcpdump_log': '/tmp/server-tcpdump-{}-{}.pcap'.format(
                 self.name, self.exp_time),
@@ -133,7 +178,11 @@ class Experiment:
         pass
             
     def run(self):
+        pass
+    
+    def _run_all_flows(self):
         with ExitStack() as stack:
+            stack.enter_context(self._run_bess())
             for flow in self.flows:
                 start_server_cmd = ('iperf3 --server '
                                     '--bind {} '
@@ -144,13 +193,48 @@ class Experiment:
                                             self.server.ip_lan,
                                             flow.server_port,
                                             1,
-                                            self.server_log)
+                                            flow.server_log)
                 start_server = RemoteCommand(start_server_cmd,
-                                             self.server_ip_addr,
-                                             logs=[self.server_log])
+                                             self.server.ip_wan,
+                                             logs=[flow.server_log])
                 stack.enter_context(start_server())
-                time.sleep(5)
+                
+            for idx, flow in enumerate(self.flows):
+                start_client_cmd = ('iperf3 --client {} '
+                                    '--port {} '
+                                    '--verbose '
+                                    '--bind {} '
+                                    '--cport {} '
+                                    '--linux-congestion {} '
+                                    '--interval 0.5 '
+                                    '--time {} '
+                                    #'--length 1024K '#1024K '
+                                    '--affinity {} '
+                                    #'--set-mss 500 ' # default is 1448
+                                    #'--window 100K '
+                                    '--zerocopy '
+                                    '--json '
+                                    '--logfile {} ').format(self.server.ip_lan,
+                                                            flow.server_port,
+                                                            self.client.ip_lan,
+                                                            flow.client_port,
+                                                            flow.ccalg,
+                                                            flow.end_time - flow.start_time,
+                                                            idx % 32,
+                                                            flow.client_log)
+                start_client = RemoteCommand(start_client_cmd,
+                                             self.client.ip_wan,
+                                             logs=[flow.client_log])
+                stack.enter_context(start_client())
+            # assume all flows start and stop at the same time
+            time.sleep(flow.end_time - flow.start_time + 5)
 
+    @contextmanager
+    def _run_bess(self):
+        try:
+            yield start_bess(self)
+        finally:
+            stop_bess()
         
     def __repr__(self):
         attrs = self.__str__()
@@ -169,7 +253,7 @@ def load_config_file(config_filename):
     return config
 
 def load_experiments(config):
-    """Create experiments from config file"""
+    """Create experiments from config file and output to config"""
     client = Host(ifname=config['client']['ifname'],
                   ip_lan=config['client']['ip_lan'],
                   ip_wan=config['client']['ip_wan'],
@@ -184,6 +268,7 @@ def load_experiments(config):
         server_port = 5201
         client_port = 5555
         for flow in experiment['flows']:
+            assert(flow['start_time'] < flow['end_time'])
             flows.append(Flow(ccalg=flow['ccalg'], start_time=flow['start_time'],
                               end_time=flow['end_time'], rtt=flow['rtt'],
                               server_port=server_port, client_port=client_port,
@@ -192,16 +277,118 @@ def load_experiments(config):
             client_port += 1
         # sort flows according to their start times so they can be run in order
         flows.sort(key=lambda x: x.start_time)
-        exp = Experiment(name=experiment_name, btlbw=experiment['btlbw'],
+        exp = Experiment(name=experiment_name,
+                         btlbw=experiment['btlbw'],
                          queue_size=experiment['queue_size'],
                          flows=flows, server=server, client=client)
         assert(experiment_name not in experiments)
-        experiments[experiment_name] = Experiment(name=experiment_name,
-                                                  btlbw=experiment['btlbw'],
-                                                  queue_size=experiment['queue_size'],
-                                                  flows=flows, server=server, client=client)
+        experiments[experiment_name] = exp
+
+        # save experiment to json file
+        with open(exp.logs['description_log'], 'w') as f:
+            json.dump(exp.__dict__, f)
     return experiments
+
+def start_bess(experiment):
+    cmd = '/opt/bess/bessctl/bessctl daemon start'
+    run_local_command(cmd)
+    cmd = ("/opt/bess/bessctl/bessctl run active-middlebox-pmd "
+           "\"CCTESTBED_EXPERIMENT_DESCRIPTION='{}'\"").format(
+               experiment.logs['description_log'])
+    run_local_command(cmd)
+
+def stop_bess():
+    cmd = '/opt/bess/bessctl/bessctl daemon stop'
+    run_local_command(cmd)
+                  
+def connect_dpdk(server, client, dpdk_driver='igb_uio'):
+    # check if DPDK already connected
+    cmd = '/opt/bess/bin/dpdk-devbind.py --status | grep drv={}'.format(dpdk_driver)
+    proc = subprocess.run(cmd, check=False, shell=True, stdout=subprocess.PIPE)
+    if proc.returncode == 0:
+        logging.info('Interfaces already connected to DPDK')
+        return server.pci, client.pci
+
+    # get pcis
+    expected_server_pci = get_interface_pci(server.ifname)
+    expected_client_pci = get_interface_pci(client.ifname)
+    if expected_server_pci is None or expected_server_pci.strip()=='':
+        return server.pci, client.pci
+    assert(expected_server_pci == server.pci)
+    assert(expected_client_pci == client.pci)
+
+    # get ips on this machine
+    server_if_ip, server_ip_mask = get_interface_ip(server.ifname)
+    client_if_ip, client_ip_mask = get_interface_ip(client.ifname)
+
+    logging.info('Server: ifname = {}, pci = {}, if_ip = {}/{}'.format(
+        server.ifname, server.pci, server_if_ip, server_ip_mask))
+    logging.info('Client: ifname = {}, pci = {}, if_ip = {}/{}'.format(
+        client.ifname, client.pci, client_if_ip, client_ip_mask))    
     
+    # make sure hugepages is started
+    cmd = 'sudo sysctl vm.nr_hugepages=1024'
+    run_local_command(cmd)
+
+    # connect NIC interfaces to DPDK
+    if dpdk_driver == 'igb_uio':
+        cmd = 'sudo modprobe uio'
+        run_local_command(cmd)
+        cmd = 'sudo insmod {}'.format('/opt/bess/deps/dpdk-17.11/build/kmod/igb_uio.ko')
+        run_local_command(cmd)
+    else:
+        cmd = 'sudo modprobe {}'.format(dpdk_driver)
+        run_local_command(cmd)
+    cmd = 'sudo ifconfig {} down'
+    run_local_command(cmd.format(server.ifname))
+    run_local_command(cmd.format(client.ifname))
+    cmd = 'sudo /opt/bess/bin/dpdk-devbind.py -b {} {}'
+    run_local_command(cmd.format(dpdk_driver, server.pci))
+    run_local_command(cmd.format(dpdk_driver, client.pci))
+    
+    return server.pci, client.pci
+
+def run_local_command(cmd):
+    """Run local command return stdout"""
+    logging.info('Running cmd: {}'.format(cmd))
+    proc = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE)
+    return proc.stdout.decode('utf-8')
+
+def get_interface_pci(ifname):
+    """Return the PCI of the given interface
+    
+    Parameters:
+    -----------
+    ifname : str
+       The interface name
+
+    Raise: ValueError if there is no interface with given name
+    """
+    cmd = '/opt/bess/bin/dpdk-devbind.py --status | grep {}'.format(ifname)
+    proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    pci = proc.stdout
+    pci = pci.split()[0][-7:]
+    if pci.strip() == '':
+        raise ValueError('There is no interface with name {}.'.format(ifname))
+    return pci.decode('utf-8')
+
+def get_interface_ip(ifname):
+    """Return the IP of the given interface.
+
+    Parameters:
+    -----------
+    ifname : str
+       Interface name
+
+    Raise: ValueError there is no interface with given name
+    """
+    cmd = 'ip addr | grep {} | grep inet'.format(ifname)
+    ip = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    if ip is None or ip.strip() == '':
+        raise ValueError('There is no interface with name {}.'.format(ifname))
+    ip, mask = ip.split()[1].split('/')
+    return ip, mask
+
 def main(args):
     #setup logging
     pass
--- test_cctestbedv2.py
+++ test_cctestbedv2.py
@@ -1,8 +1,13 @@
 import cctestbedv2 as mut
+from contextlib import ExitStack
 import pytest
 import time
 import paramiko
 import os
+import subprocess
+import json
+
+#TODO: add cleanup code for experiment description log files in /tmp/
 
 @pytest.fixture(name='config')
 def test_load_config_file():
@@ -15,13 +20,17 @@ def test_load_config_file():
     return config
 
 @pytest.fixture(name='experiment')
-def test_load_experiments(config):
+def test_load_experiments(config, request):
     experiments = mut.load_experiments(config)
     assert(len(experiments)==2)
     assert(experiments['cubic-8q'].queue_size==8)
     assert(experiments['cubic-16q'].queue_size==16)
     assert(len(experiments['cubic-8q'].flows)==1)
     assert(len(experiments['cubic-16q'].flows)==1)
+    def remove_experiment_description_log():
+        for experiment in experiments.values():
+            os.remove(experiment.logs['description_log'])
+    request.addfinalizer(remove_experiment_description_log)
     return experiments['cubic-8q']
 
 def is_remote_process_running(remote_ip, pid):
@@ -57,4 +66,46 @@ def test_remote_command(experiment):
     assert(os.path.isfile(experiment.flows[0].server_log))
     os.remove(experiment.flows[0].server_log)
 
+def test_connect_dpdk(experiment):
+    expected_server_pci, expected_client_pci = mut.connect_dpdk(
+        experiment.server, experiment.client)
+    assert(expected_server_pci == experiment.server.pci)
+    assert(expected_client_pci == experiment.client.pci)
+    
+def test_experiment_run_all_flows(experiment):
+    experiment._run_all_flows()
+    for flow in experiment.flows:
+        # check that bw looks reasonable (not less than 2mb from btlbw)
+        # and delete created files
+        with open(flow.client_log) as f:
+            client_log = json.load(f)
+        mbits_per_second = client_log['end']['sum_sent']['bits_per_second'] / 10e5
+        print('Mbps={}'.format(mbits_per_second))
+        assert(experiment.btlbw - mbits_per_second < 2) 
+        assert(os.path.isfile(flow.server_log))
+        os.remove(flow.server_log)
+        assert(os.path.isfile(flow.client_log))
+        os.remove(flow.client_log)
 
+def test_experiment_run_bess(experiment):
+    with experiment._run_bess():
+        subprocess.run(['pgrep', 'bessd'], check=True)
+        subprocess.run(['/opt/bess/bessctl/bessctl', 'show', 'pipeline'],
+                       stdout=subprocess.PIPE)
+        # test that you can ping between machines
+        cmd = ('ping -c 4 -I {} {} '
+               '| tail -1 '
+               '| awk "{{print $4}}" ').format(experiment.server.ip_lan,
+                                           experiment.client.ip_lan)
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(experiment.server.ip_wan, username='ranysha')
+        _, stdout, _ = ssh_client.exec_command(cmd)
+        # parse out avg rtt from last line of ping output
+        avg_rtt = float(stdout.readline().split('=')[-1].split('/')[1])
+        ssh_client.close()        
+        assert(avg_rtt > experiment.flows[0].rtt)
+    time.sleep(3)
+    proc = subprocess.run(['pgrep', 'bessd'])
+    assert(proc.returncode != 0)
+        
