e52d937593100e688fc56b360422d275aeed2371
rware <rware@andrew.cmu.edu>
2018-10-25 11:19:03 -0500

    Add script and test function for connecting bess

---
+++ tests/test_connect_bess.py
@@ -0,0 +1,18 @@
+import connect_bess as mut
+import subprocess
+import shlex
+import pytest
+import os
+
+@pytest.mark.parametrize("host", ['cctestbed-server','cctestbed-client'])
+def test_get_ip_wan(host):
+    ip_wan = mut.get_ip_wan(host)
+    cmd = "ssh {} ifconfig enp1s0f0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1".format(host)
+    expected_ip = subprocess.check_output(cmd, shell=True).strip().decode('utf-8')
+    assert(expected_ip == ip_wan)
+
+@pytest.mark.parametrize("host", ['cctestbed-server','cctestbed-client'])
+def test_get_key_filename(host):
+    key_filename = mut.get_key_filename(host)
+    expected_key_path = '/users/{}/.ssh/'.format(os.environ.get('USER'))
+    assert(key_filename.startswith(expected_key_path))
