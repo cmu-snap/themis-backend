24560bcf89141f7376c4e74ebc96759b36ac9b7b
rware <rware@andrew.cmu.edu>
2018-10-26 12:05:57 -0500

    Modify connect bess script to use new config script

--- connect_bess.py
+++ connect_bess.py
@@ -1,38 +1,8 @@
 from cctestbedv2 import connect_dpdk, Host
 from command import get_ssh_client, exec_command
+from config import HOST_CLIENT, HOST_SERVER
 import os
 import paramiko
-
-HOST_CLIENT = {'ifname_remote': 'enp6s0f1',
-               'ifname_local': 'enp6s0f0',
-               'ip_lan':'192.0.0.4',
-               'ip_wan':None,
-               'pci':'06:00.0',
-               'key_filename':None,
-               'username':'rware'}
-
-HOST_SERVER = {'ifname_remote':'enp6s0f1',
-               'ifname_local':'enp6s0f1',
-               'ip_lan':'192.0.0.2',
-               'ip_wan':None,
-               'pci':'06:00.1',
-               'key_filename':None,
-               'username':'rware'}
-
-def get_ssh_config():
-    ssh_config_filepath = '/users/{}/.ssh/config'.format(os.environ.get('USER'))
-    ssh_config = paramiko.config.SSHConfig()
-    with open(ssh_config_filepath) as f:
-        ssh_config.parse(f)
-    return ssh_config
-
-def get_ip_wan(host):
-    host_config = get_ssh_config().lookup(host)
-    return host_config['hostname']
-
-def get_key_filename(host):
-    host_config = get_ssh_config().lookup(host)
-    return host_config['identityfile'][0]
 
 def main():
     server_ip_wan = get_ip_wan('cctestbed-server')
