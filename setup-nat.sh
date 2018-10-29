ef3f8d4b81ef7c1a154b1e9cc618ba917bff59d4
rware <rware@andrew.cmu.edu>
2018-10-29 00:13:44 -0500

    New NAT configuration that actually works

--- setup-nat.sh
+++ setup-nat.sh
@@ -3,6 +3,5 @@ sudo iptables --table nat --flush
 sudo iptables --delete-chain
 sudo iptables --table nat --delete-chain
 echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward
-sudo iptables -t nat -A PREROUTING -i enp11s0f0 --source 35.160.118.3 -j DNAT --to-destination 192.0.0.4
-sudo iptables -t nat -A POSTROUTING --destination 192.0.0.4 -o ens13 -j SNAT --to-source 192.0.0.1
-sudo iptables -A FORWARD -i enp11s0f0 -j ACCEPT
+PUBLIC_IP=$(ifconfig enp1s0f0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1)
+sudo iptables -t nat -A POSTROUTING --source 192.0.0.2 -o enp1s0f0 -j SNAT --to $PUBLIC_IP
