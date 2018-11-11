455435690ed73e767a457f3ad9bdf15480b99d1f
rware <rware@andrew.cmu.edu>
2018-11-11 12:06:12 -0600

    Fix bug where host client template was incorrect

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import pickle
 with open('/opt/cctestbed/host_info.pkl','rb') as f:  # Python 3: open(..., 'rb')
     HOST_SERVER, HOST_CLIENT = pickle.load(f)
 
-HOST_CLIENT_TEMPLATE = HOST_SERVER._asdict()
+HOST_CLIENT_TEMPLATE = HOST_CLIENT._asdict()
 HOST_CLIENT_TEMPLATE['ip_wan'] = None
