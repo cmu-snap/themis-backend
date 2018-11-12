4f1cc6c93e2c2fdf430dec554b94fd44c607c0ce
Megan Yu <mtyu227@gmail.com>
2018-11-12 00:23:12 -0600

    Work on experiment queue functionality

--- ccalg_agent.py
+++ ccalg_agent.py
@@ -1,6 +1,30 @@
 import shlex, subprocess
 import re
+import json
+import redis
 
+redis_host = 'localhost'
+redis_port = 6379
+redis_password = ''
+
+def queue_experiment(conn, website, filename, btlbw, rtt):
+    inputs = {
+        'website': website,
+        'file': filename,
+        'btlbw': btlbw,
+        'rtt': rtt
+    }
+
+    conn.rpush('queue:experiment', json.dumps(inputs)
+
+def dequeue_experiment(conn):
+    packed = conn.blpop(['queue:experiment'], 0)
+    if packed:
+        inputs = json.loads(packed[1])
+        # TODO: Set experiment status
+        tar = run_experiment(inputs['website'], inputs['filename'], inputs['btlbw'], inputs['rtt'])
+        # TODO: Store finished experiment in database
+    
 # Call ccalg_predict.py with the given arguments and return the experiment tar filename
 def run_experiment(website, filename, btlbw, rtt):
     cmd = 'python3.6 ccalg_predict.py --website {} {} --network {} {}'
@@ -15,4 +39,9 @@ def run_experiment(website, filename, btlbw, rtt):
 
     return tar
 
+def main():
+    r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password)
+    # TODO: infinite loop dequeue_experiment
 
+if __name__ == '__main__':
+    main()
