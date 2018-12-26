255836176dca2d98815e7bcc9c600b8aed1a7fcf
rware <rware@andrew.cmu.edu>
2018-12-26 15:30:02 -0600

    Fix major bug in config that would not allow website experiments to run

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ with open('/opt/cctestbed/host_info.pkl','rb') as f:  # Python 3: open(..., 'rb'
 HOST_CLIENT_TEMPLATE = HOST_CLIENT._asdict()
 HOST_CLIENT_TEMPLATE['ip_wan'] = None
 
-HOST_AWS_TEMPLATE = HOST_CLIENT_TEMPLATE
+HOST_AWS_TEMPLATE = HOST_CLIENT_TEMPLATE.copy()
 HOST_AWS_TEMPLATE['ifname_remote'] = 'eth0'
 HOST_AWS_TEMPLATE['ip_lan'] = None
 HOST_AWS_TEMPLATE['ip_wan'] = None
