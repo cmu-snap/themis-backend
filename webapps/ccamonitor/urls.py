18bea5152e809094272719882549a16f03dd070e
Ranysha Ware <rware@andrew.cmu.edu>
2020-04-13 16:46:24 -0400

    Delete all code for refactoring

--- webapps/ccamonitor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('list-jobs', views.list_jobs, name='list-jobs'),
-    path('queue-experiment', views.queue_experiment, name='queue-experiment'),
-]
--- webapps/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ExperimentForm(forms.Form):
-    website = forms.URLField(label='Website')
-    filename = forms.URLField(label='File URL')
-    btlbw = forms.IntegerField(label='BTLBW', required='False')
-    rtt = forms.IntegerField(label='RTT', required='False')
-    queue_size = forms.IntegerField(label='Queue size', required='False')
